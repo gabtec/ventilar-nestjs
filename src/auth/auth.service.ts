3a6127cc2e88aead3d7b09df5be91e990f6c1b11
Gabriel Martins <gabtec@sapo.pt>
2023-02-21 12:02:02 +0000

    chore(): mess with auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,25 +1,32 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 // import { CryptoService } from './crypto.service';
 import { ChangePasswordDto } from './dtos/change-password.dto';
 import { LoginCredentialsDto } from './dtos/login-credentials.dto';
-import { JwtPayload } from './jwt/jwt-payload.interface';
+import { JwtPayload } from './jwt/tokens-payload.interface';
 import * as bcrypt from 'bcryptjs';
+// import * as argon2 from 'argon2';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
-    // private readonly cryptoService: CryptoService,
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
   ) {}
+
   async login(credentials: LoginCredentialsDto) {
     const { username, password } = credentials;
 
-    let mec;
+    let mec: number;
+
     try {
       mec = parseInt(username, 10);
       if (isNaN(mec)) throw new UnauthorizedException('Invalid credentials!');
@@ -73,7 +80,64 @@ export class AuthService {
     }
   }
 
+  async refreshTokens(userID: number, refreshToken: string) {
+    // TODO
+    const user = await this.usersService.getUserById(userID);
+
+    if (!user || !user.refresh_token_hash) {
+      throw new ForbiddenException('Access Denied');
+    }
+
+    if (user.refresh_token_hash !== refreshToken) {
+      throw new ForbiddenException('Access Denied');
+    }
+
+    const isValid = await this.jwtService.verifyAsync(refreshToken);
+    if (!isValid) {
+      throw new ForbiddenException('Access Denied');
+    }
+
+    // create new cookie with refreshToken
+    const tokens = await this.getTokens(user.id, user.name);
+
+    // TODO save hashed refresh token on database
+    //....
+
+    // return the new accessToken
+    return tokens;
+  }
+
   async changePassword(userID: number, changePasswDto: ChangePasswordDto) {
     return await this.usersService.changePassword(userID, changePasswDto);
+  }
+
+  async getTokens(userId: string, username: string) {
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          username,
+        },
+        {
+          secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
+          expiresIn: '15m',
+        },
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          username,
+        },
+        {
+          secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+          expiresIn: '7d',
+        },
+      ),
+    ]);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
   }
 }
