35ff33dfb8a1eafc3194347eb3097e303dacaee6
Gabriel Martins <gabtec@sapo.pt>
2023-02-28 18:15:45 +0000

    chore(): add database migrations files

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,20 +1,28 @@
-import { DataSource, DataSourceOptions } from 'typeorm/data-source';
+import 'reflect-metadata';
+import { Order } from 'src/orders/entities/order.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Ventilator } from 'src/ventilators/entities/ventilator.entity';
+import { Ward } from 'src/wards/entities/ward.entity';
+import { DataSource } from 'typeorm';
 // import * as dotenv from 'dotenv';
 
 // dotenv.config();
 
-export const dataSourceOptions: DataSourceOptions = {
+export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
   port: 5432,
   username: 'admin',
   password: 'admin',
-  database: 'ventilar_db',
-  entities: ['dist/**/*.entity.js'],
+  database: 'ventilar_db3',
+  entities: [User, Ward, Ventilator, Order],
   migrations: ['dist/db/migrations/*.js'],
+  // migrations: ['db/migrations/*.js'],
   // migrationsTableName: 'migrations',    // this is the default
   // logging: false,
-  // synchronize: true,
-};
-const dataSource = new DataSource(dataSourceOptions);
-export default dataSource;
+  synchronize: false,
+  // cli: {
+  //   migrationsDir: 'dist/db/migrations/',
+  //   // entitiesDir: 'dist/src/**/',
+  // },
+});
